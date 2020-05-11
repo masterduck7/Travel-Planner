8f91e3103d60d5bcd1753867e28f742834e6b379
Luis <luis.ramirez.12@sansano.usm.cl>
2020-05-11 16:12:04 -0400

    Trip list in table

--- frontend/src/Components/TripList.js
+++ frontend/src/Components/TripList.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
-import { Col, Icon, List, Menu, Row } from 'antd';
+import { Col, Divider, Icon, Menu, Row, Table } from 'antd';
 import {Link} from 'react-router-dom';
 
 export default class TripList extends Component {
 
     render() {
+        const columns = [
+            {
+                title: 'Destino',
+                dataIndex: 'destination',
+                key: 'destination',
+                render: destination => <a>{destination}</a>,
+            },
+            {
+                title: 'Fecha Inicio',
+                dataIndex: 'start_date',
+                key: 'start_date',
+                render: start_date => <a>{start_date}</a>,
+            },
+            {
+                title: 'Fecha Fin',
+                dataIndex: 'end_date',
+                key: 'end_date',
+                render: end_date => <a>{end_date}</a>,
+            },
+            {
+                title: 'Archivo planificación',
+                dataIndex: 'planning_file',
+                key: 'planning_file',
+                render: planning_file => <a>{planning_file}</a>,
+            },
+            {
+                title: 'Acción',
+                key: 'action',
+                render: (text, item) => (
+                  <span>
+                    <a>Editar</a>
+                    <Divider type="vertical" />
+                    <a>Eliminar</a>
+                  </span>
+                ),
+            }
+        ]
         return(
             <div>
                 <Row>
-                <Col span={6}>
+                <Col span={4}>
                     <div style={{width: 200}}>
                         <Menu
                         defaultSelectedKeys={['1']}
@@ -33,21 +70,8 @@ export default class TripList extends Component {
                         </Menu>
                     </div>
                 </Col>
-                <Col span={18}>
-                    <List
-                        style={{ marginLeft:"-10%", marginRight:"5%"}}
-                        dataSource={this.props.data}
-                        renderItem={item => (
-                            <List.Item>
-                            <List.Item.Meta
-                            title={item.destination}
-                            description={item.planning_file}
-                            />
-                            <p>{item.start_date}</p>
-                            <p>{item.end_date}</p>
-                            </List.Item>
-                        )}
-                    />
+                <Col span={19}>
+                    <Table columns={columns} dataSource={this.props.data} />
                 </Col>
                 </Row>
             </div>
