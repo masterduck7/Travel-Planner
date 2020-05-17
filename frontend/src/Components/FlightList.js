434973939f8eb1fc5a69c4bdd33b4871fc55def2
Peixe <peixe@Peixes-MacBook-Pro.local>
2020-05-17 14:29:14 -0400

    Edit and remove flight

--- frontend/src/Components/FlightList.js
+++ frontend/src/Components/FlightList.js
@@ -1,10 +1,111 @@
 import React, { Component } from 'react';
-import { Button, Col, Divider, Icon, Menu, Row, Table } from 'antd';
+import { Button, Col, DatePicker, Divider, Form, Icon, Input, Menu, Row, Table } from 'antd';
 import {Link} from 'react-router-dom';
+import { Modal } from 'react-responsive-modal';
+import moment from 'moment';
+import axios from 'axios';
+import 'react-responsive-modal/styles.css';
+import '../Assets/styles.css'
 
 export default class FlightList extends Component {
 
+    constructor(props){
+        super(props)
+        this.state = {
+            modalEdit: false,
+            modalRemove: false,
+            flight_id: "",
+            origin: "",
+            destination: "",
+            airline_name: "",
+            flight_number: "",
+            start_date: "",
+            end_date: "",
+            price: ""
+        }
+    }
+
+    onOpenModalEdit = (record) => {
+        this.setState({ 
+            modalEdit: true,
+            flight_id: record.flight_id,
+            origin: record.origin,
+            destination: record.destination,
+            airline_name: record.airline_name,
+            flight_number: record.flight_number,
+            start_date: record.start_date,
+            end_date: record.end_date,
+            price: record.price
+        });
+    };
+
+    onCloseModalEdit = () => {
+        this.setState({ modalEdit: false });
+    };
+
+    onClickEdit = event => {
+        event.preventDefault();
+        const flightID = this.state.flight_id
+        const flightObj = {
+            trip: this.props.data.tripID,
+            origin: this.state.origin,
+            destination: this.state.destination,
+            airline_name: this.state.airline_name,
+            flight_number: this.state.flight_number,
+            start_date: this.state.start_date,
+            end_date: this.state.end_date,
+            price: this.state.price
+        }
+        console.log(flightObj)
+        axios.put(`http://127.0.0.1:8000/flights/${flightID}/`, flightObj)
+        .then((response) => {
+            alert("Vuelo editado")
+            window.location.href = "/#/trips"
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    removeFlight = (event, item) => {
+        event.preventDefault();
+        const flightID = item.flight_id
+        axios.delete(`http://127.0.0.1:8000/flights/${flightID}/`)
+        .then(res => {
+            alert("Vuelo eliminado")
+            window.location.href = "/#/trips"
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
     render() {
+
+        const formItemLayout = {
+            labelCol: {
+              xs: { span: 12 },
+              sm: { span: 6 },
+            },
+            wrapperCol: {
+              xs: { span: 12 },
+              sm: { span: 12 },
+            },
+        };
+
+        const tailFormItemLayout = {
+            wrapperCol: {
+              xs: {
+                span: 12,
+                offset: 11,
+              },
+              sm: {
+                span: 12,
+                offset: 11,
+              },
+            },
+        };
+
         const columns = [
             {
                 title: 'Origen',
@@ -53,15 +154,89 @@ export default class FlightList extends Component {
                 key: 'action',
                 render: (text, item) => (
                   <span>
-                    <Link to='/'><a>Editar</a></Link>
+                    <a onClick={(e)=>{
+					    e.stopPropagation();
+					    this.onOpenModalEdit(item)}}>Editar</a>
                     <Divider type="vertical" />
-                    <a>Eliminar</a>
+                    <a onClick={(e)=>{
+					    e.stopPropagation();
+					    this.removeFlight(e, item)}}>Eliminar</a>
                   </span>
                 ),
             }
         ]
         return(
             <div>
+                <Modal open={this.state.modalEdit} onClose={this.onCloseModalEdit} classNames={{modal: 'customModal'}} center>
+                    <h1><center>Editar vuelo</center></h1>
+                    <p>
+                    <Form {...formItemLayout} onSubmit={this.onClickEdit.bind(this)} >
+                        <Form.Item label="Origen">
+                            <Input name="origin" type="text" value={this.state.origin} 
+                            onChange={(e) => {
+                                this.setState({
+                                    origin: e.target.value
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item label="Destino">
+                            <Input name="destination" type="text" value={this.state.destination} 
+                            onChange={(e) => {
+                                this.setState({
+                                    destination: e.target.value
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item label="Aerolinea">
+                            <Input name="airline_name" value={this.state.airline_name} 
+                            onChange={(e) => {
+                                this.setState({
+                                    airline_name:e.target.value
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item label="Numero de vuelo">
+                            <Input name="flight_number" value={this.state.flight_number}
+                            onChange={(value) => {
+                                this.setState({
+                                    flight_number: value
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item label="Fecha Inicio">
+                            <DatePicker name="start_date" placeholder="Ingrese fecha" format='YYYY-MM-DD'
+                            value={moment(this.state.start_date)}
+                            onChange={(value) => {
+                                this.setState({
+                                    start_date: moment(value).format('YYYY-MM-DD')
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item label="Fecha Fin">
+                            <DatePicker name="end_date" placeholder="Ingrese fecha" format='YYYY-MM-DD'
+                            value={moment(this.state.end_date)}
+                            onChange={(value) => {
+                                this.setState({
+                                    end_date: moment(value).format('YYYY-MM-DD')
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item label="Precio">
+                            <Input name="price" value={this.state.price}
+                            onChange={(value) => {
+                                this.setState({
+                                    price: value
+                                })
+                            }}/>
+                        </Form.Item>
+                        <Form.Item {...tailFormItemLayout}>
+                            <Button type="primary" htmlType="submit">
+                                Editar
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                    </p>
+                </Modal>
                 <Row>
                 <Col xs={4} sm={6} md={6} lg={86} xl={4}>
                     <div style={{width: 200}}>
@@ -89,7 +264,7 @@ export default class FlightList extends Component {
                         </Col>
                     </Row>
                     <br />
-                    <Table columns={columns} dataSource={this.props.data.flights} />
+                    <Table columns={columns} dataSource={this.props.data.flights}/>
                 </Col>
                 </Row>
             </div>
