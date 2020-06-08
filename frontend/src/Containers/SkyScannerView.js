bc8e54f4d29402a5b23d85e258b5039cc8a96a4d
= <luis.ramirez.12@sansano.usm.cl>
2020-06-08 11:19:40 -0400

    Input form and get data

--- frontend/src/Containers/SkyScannerView.js
+++ frontend/src/Containers/SkyScannerView.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CustomLayout from '../Components/CustomLayout';
 import axios from 'axios';
+import { Dropdown, Form } from 'semantic-ui-react'
 
 export default class SkyScannerView extends Component {
 
@@ -10,16 +11,19 @@ export default class SkyScannerView extends Component {
             carriers: {},
             places: {},
             dates: {},
-            trips: {}
+            trips: {},
+            origin: "",
+            destination: "",
+            currency: ""
         }
     }
 
     getData(event){
-        let currency = "USD"
         let country = "US"
         let language = "en-US"
-        let origin = "SFO"
-        let destination = "LAX"
+        let currency = this.state.currency
+        let origin = this.state.origin
+        let destination = this.state.destination
         let startDate = "anytime"
         let endDate = "anytime"
         event.preventDefault()
@@ -65,12 +69,48 @@ export default class SkyScannerView extends Component {
             })
     }
 
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+    handleSubmit = () => console.log(this.state.origin, this.state.destination)
+
     render() {
+
+        let currencies = [
+            {key: "CLP", value: "CLP", text: "Peso Chileno"},
+            {key: "USD", value: "USD", text: "Dolar"},
+            {key: "EUR", value: "EUR", text: "Euro"}
+        ]
+
         return  (
             <div>
                 <CustomLayout data={{tab: '6'}} />
-                <h1>SkyScanner</h1>
-                <button onClick={ (event) => this.getData(event)} >hola</button>
+                <h1 style={{textAlign: 'center', marginTop: 20}}>SkyScanner Data</h1>
+                <h3 style={{marginLeft: "3%", marginTop: "3%"}}>Buscador de fechas económicas:</h3>
+                <Form style={{marginLeft: "3%"}} onSubmit={(e) => this.getData(e)}>
+                    <Form.Group>
+                    <Form.Input
+                        placeholder='Origen'
+                        name='origin'
+                        value={this.state.origin}
+                        onChange={this.handleChange}
+                    />
+                    <Form.Input
+                        placeholder='Destino'
+                        name='destination'
+                        value={this.state.destination}
+                        onChange={this.handleChange}
+                    />
+                    <Dropdown
+                        placeholder='Moneda'
+                        name='currency'
+                        onChange={this.handleChange}
+                        selection
+                        floating
+                        options={currencies}
+                    />
+                    <Form.Button content='Buscar' />
+                    </Form.Group>
+                </Form>
             </div>
         )
     }
