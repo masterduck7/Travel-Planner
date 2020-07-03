e9fb3e73921335bf4a48c0f5c4e03f9aaa518fa9
= <luis.ramirez.12@sansano.usm.cl>
2020-07-03 15:02:34 -0400

    Summry data in admin view

--- frontend/src/Containers/NoPermissionView.js
+++ frontend/src/Containers/NoPermissionView.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import { Button } from 'semantic-ui-react'
-import {Link} from 'react-router-dom';
+import { Button, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 import Blocked from '../Assets/Icons/Blocked.png'
 
 export default class NoPermissionView extends Component {
     render() {
         return  (
             <div>
+                <Link to={`/admin`}>
+                    <Button negative style={{marginLeft: "1%", marginTop: "60px", marginBottom: "1%"}}>
+                        <Icon name="angle left" />
+                        Volver
+                    </Button>
+                </Link>
+                <br />
                 <img alt="" src={Blocked} style={{display:'block', marginLeft:'auto', marginRight: 'auto', marginTop: '10%'}} height="auto" width='250px' />
                 <br/><br/>
                 <h2 style={{textAlign:'center'}}>You don't have permission to view this page</h2>
