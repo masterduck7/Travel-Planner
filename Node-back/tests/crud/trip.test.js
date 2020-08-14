6d5b0e19809fb319a9a8cd6edc05b8eb9fd50999
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2020-08-14 13:12:13 -0400

    Posts with incomplete form tests

--- Node-back/tests/crud/trip.test.js
+++ Node-back/tests/crud/trip.test.js
@@ -3,79 +3,79 @@ const app = require('../../appTest')
 const http = require('http');
 
 describe('Trip tests', () => {
-  let server;
-  let tokenAuth = null;
-  let tripCreated = null;
+    let server;
+    let tokenAuth = null;
+    let tripCreated = null;
 
-  beforeAll(done => {
-    server = http.createServer(app);
-    server.listen(done);
-  });
+    beforeAll(done => {
+        server = http.createServer(app);
+        server.listen(done);
+    });
 
-  afterAll(done => {
-    server.close(done);
-  });
+    afterAll(done => {
+        server.close(done);
+    });
 
-  it('Get Token auth', async () => {
-    const res = await request(server)
-      .post('/auth/')
-      .send({
-        username: process.env.SEED_SUPERUSER_USERNAME,
-        password: process.env.SEED_SUPERUSER_PASSWORD
-      })
-    expect(res.statusCode).toEqual(201)
-    expect(res.body).toHaveProperty('accessToken')
-    tokenAuth = res.body.accessToken
-  })
+    it('Get Token auth', async () => {
+        const res = await request(server)
+        .post('/auth/')
+        .send({
+            username: process.env.SEED_SUPERUSER_USERNAME,
+            password: process.env.SEED_SUPERUSER_PASSWORD
+        })
+        expect(res.statusCode).toEqual(201)
+        expect(res.body).toHaveProperty('accessToken')
+        tokenAuth = res.body.accessToken
+    })
 
-  it('Create a new trip', async () => {
-    const res = await request(server)
-      .post('/trips')
-      .set({ Authorization: 'Bearer ' + tokenAuth })
-      .send({
-        destination: 'Test destination',
-        start_date: '2020-02-01T06:46:28.304Z',
-        end_date: '2020-03-01T06:46:28.304Z',
-        planning_file: 'test_file.xlsx',
-        status: 'ACTIVE',
-        userID: 1
-      })
-      expect(res.statusCode).toEqual(201)
-      expect(res.body).toHaveProperty('destination')
-      tripCreated = res.body.id
-  })
+    it('Create a new trip', async () => {
+        const res = await request(server)
+        .post('/trips')
+        .set({ Authorization: 'Bearer ' + tokenAuth })
+        .send({
+            destination: 'Test destination',
+            start_date: '2020-02-01T06:46:28.304Z',
+            end_date: '2020-03-01T06:46:28.304Z',
+            planning_file: 'test_file.xlsx',
+            status: 'ACTIVE',
+            userID: 1
+        })
+        expect(res.statusCode).toEqual(201)
+        expect(res.body).toHaveProperty('destination')
+        tripCreated = res.body.id
+    })
 
-  it('Get one trip', async () => {
-    const res = await request(server)
-      .get('/trips/' + tripCreated)
-      .set({ Authorization: 'Bearer ' + tokenAuth })
-      expect(res.statusCode).toEqual(200)
-      expect(res.body).toHaveProperty('destination')
-  })
+    it('Get one trip', async () => {
+        const res = await request(server)
+        .get('/trips/' + tripCreated)
+        .set({ Authorization: 'Bearer ' + tokenAuth })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('destination')
+    })
 
-  it('Edit trip', async () => {
-    const res = await request(server)
-      .put('/trips/' + tripCreated)
-      .set({ Authorization: 'Bearer ' + tokenAuth })
-      .send({
-        destination: 'Destination edited'
-      })
-      expect(res.statusCode).toEqual(200)
-      expect(res.body).toHaveProperty('message')
-      expect(res.body.message).toBe('Trip was updated successfully.')
-  })
+    it('Edit trip', async () => {
+        const res = await request(server)
+        .put('/trips/' + tripCreated)
+        .set({ Authorization: 'Bearer ' + tokenAuth })
+        .send({
+            destination: 'Destination edited'
+        })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('message')
+        expect(res.body.message).toBe('Trip was updated successfully.')
+    })
 
-  it('Remove trip', async () => {
-    const res = await request(server)
-      .delete('/trips/'+ tripCreated)
-      .set({ Authorization: 'Bearer ' + tokenAuth })
-      expect(res.statusCode).toEqual(200)
-  })
+    it('Remove trip', async () => {
+        const res = await request(server)
+        .delete('/trips/'+ tripCreated)
+        .set({ Authorization: 'Bearer ' + tokenAuth })
+        expect(res.statusCode).toEqual(200)
+    })
 
-  it('Get all trips', async () => {
-    const res = await request(server)
-      .get('/trips')
-      .set({ Authorization: 'Bearer ' + tokenAuth })
-      expect(res.statusCode).toEqual(200)
-  })
+    it('Get all trips', async () => {
+        const res = await request(server)
+        .get('/trips')
+        .set({ Authorization: 'Bearer ' + tokenAuth })
+        expect(res.statusCode).toEqual(200)
+    })
 })
